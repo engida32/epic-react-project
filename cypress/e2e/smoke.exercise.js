4c0f2e0098dc6071e8f80368143eb7264b6c120a
engida <engida32lishan@gmail.com>
2022-06-06 17:55:43 +0300

    e2e testing with cyphers

--- cypress/e2e/smoke.exercise.js
+++ cypress/e2e/smoke.exercise.js
@@ -1,8 +1,57 @@
 // 🐨 you'll want a fake user to register as:
-// import {buildUser} from '../support/generate'
+import {buildUser} from '../support/generate'
 
 describe('smoke', () => {
+  const user = buildUser()
   it('should allow a typical user flow', () => {
+    cy.visit('/')
+    cy.findByRole('button', {name: /register/i}).click()
+    cy.findByRole('dialog').within(() => {
+      cy.findByRole('textbox', {name: /username/i}).type(user.username)
+      cy.findByLabelText(/password/i).type(user.password)
+      cy.findByRole('button', {name: /register/i}).click()
+    })
+    cy.findByRole('navigation').within(() => {
+      cy.findByRole('link', {name: /discover/i}).click()
+    })
+    cy.findByRole('main').within(() => {
+      cy.findByRole('searchbox', {name: /search/i}).type('voice of war{enter}')
+      cy.findByRole('listitem', {name: /voice of war/i}).within(() => {
+        cy.findByRole('button', {name: /add to list/i}).click()
+      })
+    })
+    cy.findByRole('main').within(() => {
+      cy.findByRole('listitem').should('have.length', 1)
+      cy.findByRole('link', {name: /voice of war/i}).click()
+    })
+    cy.findByRole('main').within(() => {
+      cy.findByRole('textbox', {name: /notes/i}).type('this is a note book')
+      cy.findByLabelText(/loading/i).should('exist')
+      cy.findByLabelText(/loading/i).should('not.exist')
+      cy.findByRole('button', {name: /mark as read/i}).click()
+      cy.findByRole('radio', {name: /5 stars/i}).click({force: true})
+    })
+
+    cy.findByRole('navigation').within(() => {
+      cy.findByRole('link', {name: /finished books/i}).click()
+    })
+
+    cy.findByRole('main').within(() => {
+      cy.findAllByRole('listitem').should('have.length', 1)
+      cy.findByRole('radio', {name: /5 stars/i}).should('be.checked')
+      cy.findByRole('link', {name: /voice of war/i}).click()
+    })
+    cy.findByRole('main').within(() => {
+      cy.findByRole('button', {name: /remove from list/i}).click()
+      cy.findByRole('textbox', {name: /notes/i}).should('not.exist')
+      cy.findByRole('radio', {name: /5 stars/i}).should('not.exist')
+    })
+    cy.findByRole('navigation').within(() => {
+      cy.findByRole('link', {name: /finished books/i}).click()
+    })
+    cy.findByRole('main').within(() => {
+      cy.findByRole('listitem').should('have.length', 0)
+    })
     // 🐨 create a fake user
     // 🐨 visit '/' (📜 https://docs.cypress.io/api/commands/visit.html)
     // 🐨 find the button named "register" and click it
